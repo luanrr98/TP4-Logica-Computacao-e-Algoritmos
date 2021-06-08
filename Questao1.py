9a6929f99896c4884f349507703d8afea8865caf
Luan Dos Reis Ribeiro <luanrr98@gmail.com>
2021-06-07 22:29:40 -0300

    Corrigido print da porcentagem

--- Questao1.py
+++ Questao1.py
@@ -11,7 +11,7 @@ for contador in range(10):
     valor_compra = float(input(f"Qual é o valor de compra do {contador+1}º produto? R$"))
     valor_venda = float(input(f"Qual é o valor de venda do {contador+1}° produto? R$"))
     porcentagem = ((valor_venda-valor_compra)/valor_venda)*100
-    
+    print(f"Porcetagem do lucro {porcentagem:.2f}%")
     #Teste para ver em qual categoria se encaixa
     if porcentagem < 10:
         lucro_menor_10 = lucro_menor_10+1
@@ -19,7 +19,7 @@ for contador in range(10):
         lucro_10_20 = lucro_10_20+1
     elif porcentagem > 20:
         lucro_maior_20 = lucro_maior_20+1
-    print(f"{porcentagem:.2f}")
+    
 
 print(f"Lucro menor que 10%: {lucro_menor_10} produtos.")
 print(f"Lucro entre 10% e 20%: {lucro_10_20} produtos.")
